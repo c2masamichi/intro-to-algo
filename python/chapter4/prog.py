d9b4642d704b98209c6a4f8b0bab572a146a695e
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-02-25 22:57:20 +0900

    chapter4 by python

---
+++ python/chapter4/prog.py
@@ -0,0 +1,48 @@
+def find_max_crossing_subarray(a, low, mid, high):
+    left_sum = -float('inf')
+    sumv = 0
+    for i in range(mid, low - 1, -1):
+        sumv = sumv + a[i]
+        if sumv > left_sum:
+            left_sum = sumv
+            max_left = i
+
+    right_sum = -float('inf')
+    sumv = 0
+    for j in range(mid + 1, high + 1):
+        sumv = sumv + a[j]
+        if sumv > right_sum:
+            right_sum = sumv
+            max_right = j
+
+    return max_left, max_right, left_sum + right_sum
+
+
+def find_maximum_subarray(a, low, high):
+    if high == low:
+        return low, high, a[low]
+    else:
+        mid = (low + high) // 2
+        left_low, left_high, left_sum = find_maximum_subarray(
+            a, low, mid)
+        right_low, right_high, right_sum = find_maximum_subarray(
+            a, mid + 1, high)
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(
+            a, low, mid, high)
+
+        if left_sum >= right_sum and left_sum >= cross_sum:
+            return left_low, left_high, left_sum
+        elif right_sum >= left_sum and right_sum >= cross_sum:
+            return right_low, right_high, right_sum
+        else:
+            return cross_low, cross_high, cross_sum
+
+
+def square_matrix_multiply(a, b):
+    n = len(a)
+    c = [[0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i][j] += a[i][k] * b[k][j]
+    return c
---
+++ python/chapter4/test_prog.py
@@ -0,0 +1,106 @@
+import unittest
+
+import prog
+
+
+class TestClient(unittest.TestCase):
+
+    def test_find_max_crossing_subarray(self):
+        parameters = [
+            (
+                ([1, 2, 3, 4, 5], 0, 2, 4),
+                (0, 4, 15)
+            ),
+            (
+                ([10, -1, -1, -1, -1, -1, -1, 10], 1, 3, 6),
+                (3, 4, -2)
+            ),
+            (
+                ([-2, 3, -2, 3, 4, 2, -5, -1], 0, 3, 7),
+                (1, 5, 10)
+            ),
+        ]
+        for args, expected in parameters:
+            a, low, mid, high = args
+            with self.subTest(args=args):
+                self.assertEqual(
+                    prog.find_max_crossing_subarray(a, low, mid, high),
+                    expected
+                )
+
+    def test_find_maximum_subarray(self):
+        parameters = [
+            (
+                ([5], 0, 0),
+                (0, 0, 5)
+            ),
+            (
+                ([10, 11, 21], 1, 1),
+                (1, 1, 11)
+            ),
+            (
+                ([2, 2, 2, 2, 2], 0, 4),
+                (0, 4, 10)
+            ),
+            (
+                ([-2, -3, -1, -2, -5], 0, 4),
+                (2, 2, -1)
+            ),
+            (
+                ([-2, -2, -2, -2, -2], 0, 4),
+                (0, 0, -2)
+            ),
+            (
+                ([10, -5, -6, 5, 6, -2, -3, 2], 0, 7),
+                (3, 4, 11)
+            ),
+        ]
+        for args, expected in parameters:
+            a, low, high = args
+            with self.subTest(args=args):
+                self.assertEqual(
+                    prog.find_maximum_subarray(a, low, high),
+                    expected
+                )
+
+    def test_square_matrix_multiply(self):
+        parameters = [
+            (
+                [
+                    [1, 2],
+                    [3, 4]
+                ],
+                [
+                    [1, 0],
+                    [0, 1]
+                ],
+                [
+                    [1, 2],
+                    [3, 4]
+                ]
+            ),
+            (
+                [
+                    [1, 2],
+                    [3, 4]
+                ],
+                [
+                    [2, 1],
+                    [1, 2]
+                ],
+                [
+                    [4, 5],
+                    [10, 11]
+                ]
+            ),
+        ]
+        for a, b, expected in parameters:
+            with self.subTest(a=a, b=b):
+                self.assertEqual(
+                    prog.square_matrix_multiply(a, b),
+                    expected
+                )
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
