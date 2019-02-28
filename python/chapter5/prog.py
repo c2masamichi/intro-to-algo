deab512f19471714c15368dbeadad3367d136186
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-01 00:27:45 +0900

    permute_by_sorting (python)

---
+++ python/chapter5/prog.py
@@ -0,0 +1,17 @@
+from operator import itemgetter
+import random
+
+
+def sort_by_p(a, p):
+    pairs = list(zip(a, p))
+    pairs.sort(key=itemgetter(1))
+    for i in range(len(a)):
+        a[i] = pairs[i][0]
+
+
+def permute_by_sorting(a):
+    n = len(a)
+    p = [1] * n
+    for i in range(n):
+        p[i] = random.randint(1, n ** 3)
+    sort_by_p(a, p)
---
+++ python/chapter5/test_prog.py
@@ -0,0 +1,39 @@
+import unittest
+
+import prog
+
+
+class TestClient(unittest.TestCase):
+
+    def test_sort_by_p(self):
+        parameters = [
+            (
+                ([10], [1]),
+                [10]
+            ),
+            (
+                ([10, 20, 30], [2, 3, 1]),
+                [30, 10, 20]
+            ),
+        ]
+        for args, expected in parameters:
+            a, p = args
+            with self.subTest(args=args):
+                prog.sort_by_p(a, p),
+                self.assertEqual(a, expected)
+
+    def test_permute_by_sorting(self):
+        parameters = [
+            [3],
+            [3, 2, 5, 1],
+            [10, 2, 2, 4, 4, 3, 7],
+        ]
+        for a in parameters:
+            with self.subTest(a=a):
+                expected = a[:]
+                prog.permute_by_sorting(a)
+                self.assertCountEqual(a, expected)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
