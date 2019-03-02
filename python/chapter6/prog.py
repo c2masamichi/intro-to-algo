a8a4d3add9c826c9b49095bb2dd51ca44dbbc7a2
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-02 19:22:23 +0900

    max_heapify (python)

---
+++ python/chapter6/prog.py
@@ -0,0 +1,25 @@
+def parent(i):
+    return i // 2
+
+
+def left(i):
+    return 2 * i
+
+
+def right(i):
+    return 2 * i + 1
+
+
+def max_heapify(a, i):
+    heap_size = len(a)
+    l_index = left(i)
+    r_index = right(i)
+    if l_index <= heap_size and a[l_index] > a[i]:
+        largest = l_index
+    else:
+        largest = i
+    if r_index <= heap_size and a[r_index] > a[largest]:
+        largest = r_index
+    if largest != i:
+        a[i], a[largest] = a[largest], a[i]
+        max_heapify(a, largest)
---
+++ python/chapter6/test_prog.py
@@ -0,0 +1,54 @@
+import unittest
+
+import prog
+
+
+class TestClient(unittest.TestCase):
+
+    def test_parent(self):
+        parameters = [
+            (4, 2),
+            (5, 2),
+        ]
+        for v, expected in parameters:
+            with self.subTest(v=v):
+                self.assertEqual(prog.parent(v), expected)
+
+    def test_left(self):
+        parameters = [
+            (1, 2),
+            (2, 4),
+        ]
+        for v, expected in parameters:
+            with self.subTest(v=v):
+                self.assertEqual(prog.left(v), expected)
+
+    def test_right(self):
+        parameters = [
+            (1, 3),
+            (2, 5),
+        ]
+        for v, expected in parameters:
+            with self.subTest(v=v):
+                self.assertEqual(prog.right(v), expected)
+
+    def test_max_heapify(self):
+            parameters = [
+                (
+                    ([-1, 4, 5, 3, 2, 1], 1),
+                    [-1, 5, 4, 3, 2, 1]
+                ),
+                (
+                    ([-1, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1], 2),
+                    [-1, 16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+                ),
+            ]
+            for args, expected in parameters:
+                a, i = args
+                with self.subTest(args=args):
+                    prog.max_heapify(a, i),
+                    self.assertEqual(a, expected)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
