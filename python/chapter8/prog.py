199528ad6c246b58cbfddadb0a7a749ba5ca9a07
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-09 17:40:59 +0900

    radix_sort (python)

--- python/chapter8/prog.py
+++ python/chapter8/prog.py
@@ -7,3 +7,26 @@ def counting_sort(a, b, k):
     for j in range(len(a) - 1, -1, -1):
         b[c[a[j]] - 1] = a[j]
         c[a[j]] -= 1
+
+
+def counting_sort_by_digit(a, d):
+    b = [0] * len(a)
+    k = 10
+    div = 10 ** (d-1)
+    c = [0] * k
+    for j in range(len(a)):
+        r = (a[j] // div) % 10
+        c[r] += 1
+    for i in range(1, k):
+        c[i] += c[i-1]
+    for j in range(len(a) - 1, -1, -1):
+        r = (a[j] // div) % 10
+        b[c[r] - 1] = a[j]
+        c[r] -= 1
+    return b
+
+
+def radix_sort(a, d):
+    for i in range(1, d+1):
+        a = counting_sort_by_digit(a, i)
+    return a
--- python/chapter8/test_prog.py
+++ python/chapter8/test_prog.py
@@ -22,6 +22,48 @@ class TestClient(unittest.TestCase):
                 prog.counting_sort(a, b, k)
                 self.assertEqual(b, expected)
 
+    def test_counting_sort_by_digit(self):
+        parameters = [
+            (
+                ([53, 90, 91, 12], 1),
+                [90, 91, 12, 53]
+            ),
+            (
+                ([90, 91, 12, 53], 2),
+                [12, 53, 90, 91]
+            ),
+            (
+                ([534, 504, 415, 193, 612, 652, 302, 787, 482], 2),
+                [504, 302, 415, 612, 534, 652, 787, 482, 193]
+            ),
+        ]
+        for args, expected in parameters:
+            a, d = args
+            with self.subTest(expected=expected):
+                self.assertEqual(
+                    prog.counting_sort_by_digit(a, d), expected)
+
+    def test_radix_sort(self):
+        parameters = [
+            (
+                ([3, 0, 7, 9, 1], 1),
+                [0, 1, 3, 7, 9]
+            ),
+            (
+                ([90, 91, 12, 53], 2),
+                [12, 53, 90, 91]
+            ),
+            (
+                ([534, 504, 415, 193, 612, 652, 302, 787, 482], 3),
+                [193, 302, 415, 482, 504, 534, 612, 652, 787]
+            ),
+        ]
+        for args, expected in parameters:
+            a, d = args
+            with self.subTest(expected=expected):
+                self.assertEqual(
+                    prog.radix_sort(a, d), expected)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
