faf3e5e29926cbe8dacd2ad8b38fa7a4a81fdf16
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-10 22:35:15 +0900

    randomized_select (python)

--- python/chapter9/prog.py
+++ python/chapter9/prog.py
@@ -1,6 +1,39 @@
+import random
+
+
 def minimum(a):
     minv = a[0]
     for i in range(1, len(a)):
         if minv > a[i]:
             minv = a[i]
     return minv
+
+
+def partition(a, p, r):
+    x = a[r]
+    i = p - 1
+    for j in range(p, r):
+        if a[j] <= x:
+            i += 1
+            a[i], a[j] = a[j], a[i]
+    a[i+1], a[r] = a[r], a[i+1]
+    return i + 1
+
+
+def randomized_partition(a, p, r):
+    i = random.randint(p, r)
+    a[r], a[i] = a[r], a[i]
+    return partition(a, p, r)
+
+
+def randomized_select(a, p, r, i):
+    if p == r:
+        return a[p]
+    q = randomized_partition(a, p, r)
+    k = q - p + 1
+    if i == k:
+        return a[q]
+    elif i < k:
+        return randomized_select(a, p, q - 1, i)
+    else:
+        return randomized_select(a, q + 1, r, i - k)
--- python/chapter9/test_prog.py
+++ python/chapter9/test_prog.py
@@ -14,6 +14,57 @@ class TestClient(unittest.TestCase):
             with self.subTest(expected=expected):
                 self.assertEqual(prog.minimum(a), expected)
 
+    def test_partition(self):
+        parameters = [
+            (
+                ([4, 3, 1, 2], 0, 3),
+                ([1, 2, 4, 3], 1)
+            ),
+            (
+                ([2, 8, 7, 1, 3, 5, 6, 4], 0, 7),
+                ([2, 1, 3, 4, 7, 5, 6, 8], 3)
+            ),
+        ]
+        for args, expected in parameters:
+            a, p, r = args
+            after, q = expected
+            with self.subTest(expected=expected):
+                self.assertEqual(prog.partition(a, p, r), q)
+                self.assertEqual(a, after)
+
+    def test_randomized_partition(self):
+        parameters = [
+            (
+                ([4, 3, 1, 2], 0, 3),
+                (1, 2, 3, 4)
+            ),
+            (
+                ([2, 8, 7, 1, 3, 5, 6, 4], 0, 7),
+                (1, 2, 3, 4, 5, 6, 7, 8)
+            ),
+        ]
+        for args, expected in parameters:
+            a, p, r = args
+            with self.subTest(expected=expected):
+                self.assertIn(prog.partition(a, p, r), expected)
+
+    def test_randomized_select(self):
+        parameters = [
+            (
+                ([40, 30, 10, 20], 0, 3, 2),
+                20
+            ),
+            (
+                ([2, 8, 7, 1, 3, 5, 6, 4], 0, 7, 5),
+                5
+            ),
+        ]
+        for args, expected in parameters:
+            a, p, r, i = args
+            with self.subTest(expected=expected):
+                self.assertEqual(
+                    prog.randomized_select(a, p, r, i), expected)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
