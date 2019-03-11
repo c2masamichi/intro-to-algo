602b4da204ebc6dfd35ee2332175a75780be3520
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-11 23:10:17 +0900

    define Node class (python)

---
+++ python/chapter12/prog.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, key, parent=None, left=None, right=None):
+        self.key = key
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+def inorder_tree_walk(a):
+    pass
