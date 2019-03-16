ed6a319165199164c96610c8146bc46010ddb49d
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-03-16 16:24:52 +0900

    fix ttest case

--- python/chapter12/test_prog.py
+++ python/chapter12/test_prog.py
@@ -6,19 +6,21 @@ from prog import Node, Tree
 
 class TestClient(unittest.TestCase):
 
+    def make_nodes(self, root_key, left_key, right_key):
+        root = Node(root_key)
+        root.left = Node(left_key, parent=root)
+        root.right = Node(right_key, parent=root)
+        return root
+
     def test_inorder_tree_walk(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         expected = [5, 6, 7]
         result = []
         prog.inorder_tree_walk(root, result)
         self.assertEqual(result, expected)
 
     def test_tree_search(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         parameters = [
             ((root, 6), root),
             ((root, 5), root.left),
@@ -31,9 +33,7 @@ class TestClient(unittest.TestCase):
                 self.assertIs(prog.tree_search(x, k), expected)
 
     def test_iterative_tree_search(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         parameters = [
             ((root, 6), root),
             ((root, 5), root.left),
@@ -46,25 +46,19 @@ class TestClient(unittest.TestCase):
                 self.assertIs(prog.iterative_tree_search(x, k), expected)
 
     def test_tree_minimum(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         expected = 5
         result = prog.tree_minimum(root)
         self.assertEqual(result.key, expected)
 
     def test_tree_maximum(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         expected = 7
         result = prog.tree_maximum(root)
         self.assertEqual(result.key, expected)
 
     def test_tree_successor(self):
-        root = Node(6)
-        root.left = Node(5, parent=root)
-        root.right = Node(7, parent=root)
+        root = self.make_nodes(6, 5, 7)
         parameters = [
             (root, root.right),
             (root.left, root),
@@ -75,10 +69,7 @@ class TestClient(unittest.TestCase):
                 self.assertIs(prog.tree_successor(x), expected)
 
     def test_tree_insert(self):
-        root = Node(3)
-        root.left = Node(1, parent=root)
-        root.right = Node(5, parent=root)
-        t = Tree(root)
+        t = Tree(self.make_nodes(3, 1, 5))
         prog.tree_insert(t, Node(2))
         expected = [1, 2, 3, 5]
         result = []
