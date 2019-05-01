6c6a8f4805ca726fea6bfe713a8a6784b9fa626d
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-05-01 23:39:10 +0900

    add test case

--- go/chapter02/prog_test.go
+++ go/chapter02/prog_test.go
@@ -6,10 +6,19 @@ import (
 )
 
 func TestInsertionSort(t *testing.T) {
-	a := []int{3, 4, 1, 2}
-	insertionSort(a)
-	expected := []int{1, 2, 3, 4}
-	if !reflect.DeepEqual(a, expected) {
-		t.Errorf("actual: %v\nexpected: %v", a, expected)
+	parameters := []struct {
+		a        []int
+		expected []int
+	}{
+		{[]int{3}, []int{3}},
+		{[]int{3, 4, 1, 2}, []int{1, 2, 3, 4}},
+		{[]int{2, 5, 4, 1, 3, 2, 3}, []int{1, 2, 2, 3, 3, 4, 5}},
+	}
+
+	for i, p := range parameters {
+		insertionSort(p.a)
+		if !reflect.DeepEqual(p.a, p.expected) {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, p.a, p.expected)
+		}
 	}
 }
