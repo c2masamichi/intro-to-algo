a34e15b1bf938de4a7ec1c66d859fdc492433ea1
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-13 00:15:22 +0900

    chapter2 by go

--- go/chapter02/prog.go
+++ go/chapter02/prog.go
@@ -1,6 +1,10 @@
 package chapter02
 
-func insertionSort(a []int) {
+import (
+	"math"
+)
+
+func insertionSort(a []float64) {
 	for j := 1; j < len(a); j++ {
 		key := a[j]
 		i := j - 1
@@ -9,5 +13,40 @@ func insertionSort(a []int) {
 			i--
 		}
 		a[i+1] = key
+	}
+}
+
+func merge(a []float64, p, q, r int) {
+	n1 := q - p + 1
+	n2 := r - q
+	left := make([]float64, n1+1)
+	right := make([]float64, n2+1)
+	for i := 0; i < n1; i++ {
+		left[i] = a[p+i]
+	}
+	for j := 0; j < n2; j++ {
+		right[j] = a[q+j+1]
+	}
+	left[n1] = math.Inf(0)
+	right[n2] = math.Inf(0)
+	i := 0
+	j := 0
+	for k := p; k <= r; k++ {
+		if left[i] <= right[j] {
+			a[k] = left[i]
+			i++
+		} else {
+			a[k] = right[j]
+			j++
+		}
+	}
+}
+
+func mergeSort(a []float64, p, r int) {
+	if p < r {
+		q := (p + r) / 2
+		mergeSort(a, p, q)
+		mergeSort(a, q+1, r)
+		merge(a, p, q, r)
 	}
 }
--- go/chapter02/prog_test.go
+++ go/chapter02/prog_test.go
@@ -7,18 +7,60 @@ import (
 
 func TestInsertionSort(t *testing.T) {
 	parameters := []struct {
-		a        []int
-		expected []int
+		a        []float64
+		expected []float64
 	}{
-		{[]int{3}, []int{3}},
-		{[]int{3, 4, 1, 2}, []int{1, 2, 3, 4}},
-		{[]int{2, 5, 4, 1, 3, 2, 3}, []int{1, 2, 2, 3, 3, 4, 5}},
+		{[]float64{3}, []float64{3}},
+		{[]float64{3, 4, 1, 2}, []float64{1, 2, 3, 4}},
+		{[]float64{2, 5, 4, 1, 3, 2, 3}, []float64{1, 2, 2, 3, 3, 4, 5}},
 	}
 
-	for i, p := range parameters {
-		insertionSort(p.a)
-		if !reflect.DeepEqual(p.a, p.expected) {
-			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, p.a, p.expected)
+	for i, param := range parameters {
+		a, expected := param.a, param.expected
+		insertionSort(a)
+		if !reflect.DeepEqual(a, expected) {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, a, expected)
+		}
+	}
+}
+
+func TestMerge(t *testing.T) {
+	parameters := []struct {
+		a        []float64
+		p        int
+		q        int
+		r        int
+		expected []float64
+	}{
+		{[]float64{3, 4, 1, 2}, 0, 1, 3, []float64{1, 2, 3, 4}},
+		{[]float64{5, 8, 6, 7, 3, 4, 1, 2}, 0, 1, 3, []float64{5, 6, 7, 8, 3, 4, 1, 2}},
+		{[]float64{5, 8, 6, 7, 3, 4, 1, 2}, 4, 5, 7, []float64{5, 8, 6, 7, 1, 2, 3, 4}},
+	}
+
+	for i, param := range parameters {
+		a, p, q, r, expected := param.a, param.p, param.q, param.r, param.expected
+		merge(a, p, q, r)
+		if !reflect.DeepEqual(a, expected) {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, a, expected)
+		}
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	parameters := []struct {
+		a        []float64
+		expected []float64
+	}{
+		{[]float64{3}, []float64{3}},
+		{[]float64{3, 4, 1, 2}, []float64{1, 2, 3, 4}},
+		{[]float64{2, 5, 4, 1, 3, 2, 3}, []float64{1, 2, 2, 3, 3, 4, 5}},
+	}
+
+	for i, param := range parameters {
+		a, expected := param.a, param.expected
+		mergeSort(a, 0, len(a)-1)
+		if !reflect.DeepEqual(a, expected) {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, a, expected)
 		}
 	}
 }
