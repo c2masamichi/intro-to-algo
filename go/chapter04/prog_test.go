5c79bbcb0cc61ec733d1cdfffdf2df3c9f9cc794
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-15 16:24:36 +0900

    correct to lowercase

--- go/chapter04/prog_test.go
+++ go/chapter04/prog_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestFindMaxCrossingSubarray(t *testing.T) {
-	type Result struct {
+	type result struct {
 		left  int
 		right int
 		sum   float64
@@ -16,26 +16,26 @@ func TestFindMaxCrossingSubarray(t *testing.T) {
 		low      int
 		mid      int
 		high     int
-		expected Result
+		expected result
 	}{
 		{
 			[]float64{1, 2, 3, 4, 5}, 0, 2, 4,
-			Result{0, 4, 15},
+			result{0, 4, 15},
 		},
 		{
 			[]float64{10, -1, -1, -1, -1, -1, -1, 10}, 1, 3, 6,
-			Result{3, 4, -2},
+			result{3, 4, -2},
 		},
 		{
 			[]float64{-2, 3, -2, 3, 4, 2, -5, -1}, 0, 3, 7,
-			Result{1, 5, 10},
+			result{1, 5, 10},
 		},
 	}
 
 	for i, param := range parameters {
 		a, low, mid, high, expected := param.a, param.low, param.mid, param.high, param.expected
 		left, right, sum := findMaxCrossingSubarray(a, low, mid, high)
-		actual := Result{left, right, sum}
+		actual := result{left, right, sum}
 		if actual != expected {
 			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
 		}
@@ -43,7 +43,7 @@ func TestFindMaxCrossingSubarray(t *testing.T) {
 }
 
 func TestFindMaximumSubarray(t *testing.T) {
-	type Result struct {
+	type result struct {
 		left  int
 		right int
 		sum   float64
@@ -52,38 +52,38 @@ func TestFindMaximumSubarray(t *testing.T) {
 		a        []float64
 		low      int
 		high     int
-		expected Result
+		expected result
 	}{
 		{
 			[]float64{5}, 0, 0,
-			Result{0, 0, 5},
+			result{0, 0, 5},
 		},
 		{
 			[]float64{10, 11, 21}, 1, 1,
-			Result{1, 1, 11},
+			result{1, 1, 11},
 		},
 		{
 			[]float64{2, 2, 2, 2, 2}, 0, 4,
-			Result{0, 4, 10},
+			result{0, 4, 10},
 		},
 		{
 			[]float64{-2, -3, -1, -2, -5}, 0, 4,
-			Result{2, 2, -1},
+			result{2, 2, -1},
 		},
 		{
 			[]float64{-2, -2, -2, -2, -2}, 0, 4,
-			Result{0, 0, -2},
+			result{0, 0, -2},
 		},
 		{
 			[]float64{10, -5, -6, 5, 6, -2, -3, 2}, 0, 7,
-			Result{3, 4, 11},
+			result{3, 4, 11},
 		},
 	}
 
 	for i, param := range parameters {
 		a, low, high, expected := param.a, param.low, param.high, param.expected
 		left, right, sum := findMaximumSubarray(a, low, high)
-		actual := Result{left, right, sum}
+		actual := result{left, right, sum}
 		if actual != expected {
 			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
 		}
