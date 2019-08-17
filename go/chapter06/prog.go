074e80d301eb36b24f07829432b78a8ebea88cb4
乳井昌道 <19793735+c2masamichi@users.noreply.github.com>
2019-08-17 16:10:27 +0900

    parent,left,right (go)

---
+++ go/chapter06/prog.go
@@ -0,0 +1,13 @@
+package chapter06
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func left(i int) int {
+	return 2*i + 1
+}
+
+func right(i int) int {
+	return 2 * (i + 1)
+}
---
+++ go/chapter06/prog_test.go
@@ -0,0 +1,61 @@
+package chapter06
+
+import (
+	"testing"
+)
+
+func TestParent(t *testing.T) {
+	parameters := []struct {
+		k        int
+		expected int
+	}{
+		{3, 1},
+		{4, 1},
+	}
+
+	for i, param := range parameters {
+		k, expected := param.k, param.expected
+		actual := parent(k)
+		if actual != expected {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
+		}
+	}
+}
+
+func TestLeft(t *testing.T) {
+	parameters := []struct {
+		k        int
+		expected int
+	}{
+		{0, 1},
+		{1, 3},
+		{2, 5},
+	}
+
+	for i, param := range parameters {
+		k, expected := param.k, param.expected
+		actual := left(k)
+		if actual != expected {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
+		}
+	}
+}
+
+func TestRight(t *testing.T) {
+	parameters := []struct {
+		k        int
+		expected int
+	}{
+		{0, 2},
+		{1, 4},
+		{2, 6},
+	}
+
+	for i, param := range parameters {
+		k, expected := param.k, param.expected
+		actual := right(k)
+		if actual != expected {
+			t.Errorf("i: %d\nactual: %v\nexpected: %v", i, actual, expected)
+		}
+	}
+}
